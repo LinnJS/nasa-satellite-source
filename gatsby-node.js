9caa600180213103c018be7890e4512f69641f26
LinnJS <justinslinn727@gmail.com>
2020-04-09 18:52:43 -0400

    Patch sourcing node error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,11 @@
-const axios = require("axios")
-
+// Exports as sourceNodes for gatsby to store in GraphQL
 exports.sourceNodes = async ({
-                               actions,
-                               createNodeId,
-                               createContentDigest,
-                             }) => {
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+
+  // getting all satellites from the observatory
   const observatories = await axios({
     method: "GET",
     url: "https://sscweb.sci.gsfc.nasa.gov/WS/sscr/2/observatories",
@@ -15,6 +16,7 @@ exports.sourceNodes = async ({
     console.error("observatories GET error: ", error.message)
   })
 
+    // takes in resourceId to get more information about satellite
   const getDetails = async resId => {
     const details = await axios({
       method: "GET",
@@ -23,10 +25,10 @@ exports.sourceNodes = async ({
         Accept: "application/json",
       },
     })
+
     const isObservatoryRegionNull = details.data.Location
       ? details.data.Location.ObservatoryRegion
       : null
-
     const observatoryRegion = Array.isArray(isObservatoryRegionNull)
       ? isObservatoryRegionNull
       : [isObservatoryRegionNull]
@@ -42,8 +44,9 @@ exports.sourceNodes = async ({
     }
   }
 
-  const promise1 = new Promise(resolve => {
+  const promise1 = new Promise((resolve, reject) => {
     observatories.data.Observatory[1].forEach(satellites => {
+      // iterates over each satellite to get details and returns via promise
       const {
         Id,
         EndTime,
@@ -52,32 +55,44 @@ exports.sourceNodes = async ({
         Resolution,
         ResourceId,
       } = satellites
+      // console.log({ ResourceId, Resolution, StartTime, EndTime, Name, Id })
+      // console.log({ Name })
+      const hasResourceId = ResourceId ? ResourceId : null
 
-      if (ResourceId) {
-        const details = getDetails(ResourceId)
+      if (hasResourceId) {
+        const details = getDetails(hasResourceId)
+        // console.log({ details })
+        // https://nssdc.gsfc.nasa.gov/thumbnail/spacecraft/ace1.gif
         if (details) {
-          details.then(res => {
-            const node = {
-              nameID: Id,
-              resolution: Resolution,
-              endTime: EndTime[1],
-              startTime: StartTime[1],
-              name: Name,
-              resourceId: ResourceId,
-              details: res,
-              id: createNodeId(satellites.Id),
-              internal: {
-                type: "satellite",
-                contentDigest: createContentDigest(satellites),
-              },
-            }
+          details
+            .then(result => {
+              const node = {
+                nameID: Id,
+                resolution: Resolution,
+                endTime: EndTime[1],
+                startTime: StartTime[1],
+                name: Name,
+                resourceId: ResourceId,
+                details: result,
+                id: createNodeId(satellites.Id),
+                internal: {
+                  type: "satellite",
+                  contentDigest: createContentDigest(satellites),
+                },
+              }
+              actions.createNode(node)
 
-            actions.createNode(node)
-            if (res.description) resolve()
-          })
+              if (result) resolve()
+            })
+            .catch(error => {
+              console.log("details error:", error)
+              reject()
+            })
         }
       }
     })
+    resolve()
+    return promise1
   })
-  return Promise.all([promise1])
+  return promise1
 }
